1298eeb7019a160e86256c2c9b689af550ff8c52
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-03-02 21:28:22 +0200

    Updates

---
+++ precache-manifest.8d7ed344f37ab36d633dc397a67a56c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b3776c289e173d35533566eb6e544c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b1e90285344dda55470",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b9ab41a1cd313e403f95",
+    "url": "./static/js/2.a0138fc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.a0138fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1e90285344dda55470",
+    "url": "./static/js/main.079ba94c.chunk.js"
+  },
+  {
+    "revision": "2e3a6f278567b71a3edc",
+    "url": "./static/js/runtime-main.5e540f5e.js"
+  }
+]);
